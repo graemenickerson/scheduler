cc35974cc03ab08767d38e562f4b3688e703229b
Graeme <graeme.nickerson@gmail.com>
2019-10-30 14:37:48 -0700

    Adds Custom hook for view history.

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,6 +1,25 @@
 import { useState } from 'react';
 
 export default function useVisualMode (input) {
-  const [state, setState] = useState({mode : input});
-  return state;
-}
+  const [mode, setMode] = useState(input);
+  const [history, setHistory] = useState([input]);
+
+  const transition = (newMode, replace = false) => {
+    if (replace === false) {
+      const addHistory = [...history, newMode];
+      setHistory(addHistory);
+    }
+    setMode(newMode);
+  };
+  
+  const back = () => {
+    if (history.length > 1) {
+      const popHistory = [...history];
+      popHistory.pop();
+      setHistory(popHistory);
+      setMode(popHistory[popHistory.length - 1]);
+    }
+  };
+
+  return { mode, transition, back};
+};
